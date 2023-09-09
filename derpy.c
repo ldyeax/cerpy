16cd94457573a9c7d087a4a88c67ac7920938751
AppleDash <pet@feather.horse>
2023-09-09 19:42:28 -0400

    fix: exit properly on signal

--- derpy.c
+++ derpy.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 #include "conn.h"
 #include "parse.h"
 
@@ -7,6 +9,23 @@
 #define IRC_REAL "armchair"
 
 struct irc_conn *g_Conn;
+int g_Running = 1;
+
+#define UNUSED(P) ((void) P)
+
+
+static void signal_handler(int signal) {
+    UNUSED(signal);
+
+    g_Running = 0;
+
+    if (g_Conn != NULL && g_Conn->fd != -1) {
+        close(g_Conn->fd);
+        g_Conn->fd = -1;
+    }
+
+    fprintf(stderr, "Signal received; terminating\n");
+}
 
 void process_line(struct irc_line *ln) {
     if (!strcmp(ln->command, "PING")) {
@@ -31,6 +50,11 @@ int process_lines() {
 }
 
 int main(int argc, char *argv[]) {
+    UNUSED(argc); UNUSED(argv);
+    
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
     if ((g_Conn = irc_conn_open("irc.canternet.org", 6667)) == NULL) {
         return 1;
     }
@@ -39,13 +63,15 @@ int main(int argc, char *argv[]) {
     irc_conn_sendf(g_Conn, "NICK %s\r\n", IRC_NICK);
     irc_conn_sendf(g_Conn, "USER %s * * :%s\r\n", IRC_USER, IRC_REAL);
 
-    while (1) {
+    while (g_Running) {
         if (irc_conn_do_io(g_Conn) != 0) {
             break;
         }
 
         process_lines();
     }
+
+    irc_conn_close_free(g_Conn);
 
     return 0;
 }
